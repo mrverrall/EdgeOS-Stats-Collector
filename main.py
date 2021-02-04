d0e35a9278037386abac1107f6ca18819b24015e
Paul Verrall <mrverrall@gmail.com>
2021-02-04 20:43:12 +0000

    usage example

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import edgeos
+import asyncio
+import promethus_edgeos_metrics
+
+
+def main():
+
+    # set these
+    server = "<ip address>"
+    username = "<user>"
+    password = "<password>"
+    subscriptions = ["interfaces", 'system-stats']
+
+    # derive these
+    auth_params = {'username': username, 'password': password}
+
+    websocket_exporter = edgeos.WS(
+        server,
+        auth_params,
+        subscriptions,
+        promethus_edgeos_metrics.register_edgeos_metrics
+    )
+
+    asyncio.run(websocket_exporter.collect_edgeos_metrics())
+
+
+if __name__ == "__main__":
+    main()
+
