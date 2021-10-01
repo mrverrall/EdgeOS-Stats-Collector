19a3eb781586e64a75631fdc0a9d6057caf32020
Paul Verrall <mrverrall@gmail.com>
2021-10-01 18:08:16 +0100

    Block and recover on session resets

--- edgeos.py
+++ edgeos.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-import asyncio
 import json
 import requests
 import ssl
@@ -19,9 +18,9 @@ class Session():
         self._creds = credetials
 
         self._last_heartbeat = None
-        self._observers = []
+        self._req_session = None
         self._session_id = None
-        self._session = None
+        self._active = threading.Event()
 
         if isinstance(heartbeat_interval, int):
             self._heartbeat_interval = heartbeat_interval
@@ -30,9 +29,8 @@ class Session():
 
     def start_session(self):
         "Start an session ephemeral session that will not re-establish, raises an exception on fail"
-        self._session = self._get_new_session()
 
-        if self._session_id:
+        if self._set_session():
             t = threading.Thread(target=self._heartbeat)
             t.start()
         else:
@@ -40,21 +38,24 @@ class Session():
 
     def start_persistant_session(self):
         "Start a session resistant to error which will continue to retry regardless"
+
         t = threading.Thread(target=self._persistant_session)
         t.start()
 
+        self._active.wait()
+
     def _persistant_session(self):
-        "Check for session every second and try to create a new one if needed"
+        "Check for session every 5 seconds and try to create a new one if needed"
         while True:
-            try:
-                if not self.session_id:
+            if self._active.is_set():
+                time.sleep(5)
+            else:
+                try:
                     self.start_session()
-            except Exception:
-                pass
+                except Exception:
+                    pass
 
-            time.sleep(1)
-
-    def _get_new_session(self) -> requests.session:
+    def _set_session(self) -> bool:
         "Authenticate a new session with an EdgeOS device"
         session = requests.session()
 
@@ -62,13 +63,17 @@ class Session():
         ar = session.post(self._base_uri, data=self._creds, verify=False, allow_redirects=False)
 
         if ar.status_code == 303:
+            self._active.set()
+            self._req_session = session
             self.session_id = ar.cookies.get('PHPSESSID')
-            return session
 
-        return None
+            return True
+
+        return False
 
     def _destroy_session(self):
         "Destroy any existing session properties"
+        self._active.clear()
         self.session_id = None
         self._last_heartbeat = None
 
@@ -76,62 +81,45 @@ class Session():
         "Send a keepalive heartbeat to the existing session and tear it down if it ends"
         while True:
             try:
-                self._last_heartbeat = self._session.get(self._heartbeat_uri, verify=False)
+                self._last_heartbeat = self._req_session.get(self._heartbeat_uri, verify=False)
                 if self._last_heartbeat.status_code != 200:
                     raise Exception(f'Last heartbeat: {self._last_heartbeat.status_code}')
-            except ConnectionError:
-                # don't panic, try and reuse the session when this come back...
-                pass
-            except Exception:
+            except Exception as e:
                 # most likely the session has expired, our job is done.
+                print(f"Heartbeat Error: {e}")
                 self._destroy_session()
                 return
 
             time.sleep(self._heartbeat_interval)
 
     @property
-    def last_heartbeat(self) -> int:
-        "The status code returned from the last heartbeat"
-        return self._last_heartbeat
-
-    @property
     def session_id(self) -> str:
         "Current session_id"
+        self._active.wait()
         return self._session_id
 
     @session_id.setter
     def session_id(self, value):
         "This should not be set externally"
         self._session_id = value
-        self.notitify_subscribers()
-
-    def notitify_subscribers(self):
-        if self._session_id:
-            for callback in self._observers:
-                callback(self._session_id)
-
-    def subscribe_to_session_id(self, callback):
-        self._observers.append(callback)
 
 
 class WS():
 
     def __init__(self, server: str, credetials: dict, subscriptions: list, callback=None):
         self._server = server
-        self._websocket = None
+
         self._callback = callback
         self._subscriptions = subscriptions
 
         self._session = Session(server, credetials)
         self._session.start_persistant_session()
-        self._session.subscribe_to_session_id(self.session_subscriber)
 
         self._websocket = None
 
-    def session_subscriber(self, session_id):
-        asyncio.run(self._subscribe_to_ws_stats(session_id))
+    async def _subscribe_to_ws_stats(self):
 
-    async def _subscribe_to_ws_stats(self, session_id):
+        session_id = self._session.session_id
 
         if self._websocket and not self._websocket.closed and session_id:
             subs = ",".join([f'{{"name":"{sub}"}}' for sub in self._subscriptions])
@@ -146,7 +134,8 @@ class WS():
         while True:
             try:
                 async with websockets.connect(ws_uri, ssl=ssl_unverified()) as self._websocket:
-                    self._session.notitify_subscribers()
+
+                    await self._subscribe_to_ws_stats()
                     async for message in self._websocket:
                         try:
                             if j_msg := json.loads(message[message.find('{'):]):
